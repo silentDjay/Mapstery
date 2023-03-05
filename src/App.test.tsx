89d5dc36f72475d30f9ee35692342c55a4e48941
Jeremy Smith <jsmith@lulu.com>
2023-03-05 16:24:30 +0100

    refactor app to use vite instead of CRA; update tests and add to CI pipeline

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,25 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 import { App } from "./App";
 
-test("renders learn react link", () => {
+test("renders Mapstery heading", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+
+  const linkElement = screen.getByText("MAPSTERY");
   expect(linkElement).toBeInTheDocument();
+});
+
+test("initiate countries game ", () => {
+  render(<App />);
+
+  const countriesGameButton = screen.getByText("Countries of the World");
+  userEvent.click(countriesGameButton);
+
+  const letsGoButton = screen.getByText("Let's Go!");
+  expect(letsGoButton).toBeInTheDocument;
+  userEvent.click(letsGoButton);
+
+  const gameplayButton = screen.getByText("Hint");
+  expect(gameplayButton).toBeInTheDocument;
 });
