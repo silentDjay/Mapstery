849431ff7d7aef7d9fcc5886fa842c94ecea6908
Jeremy Smith <jsmith@lulu.com>
2024-03-06 23:54:03 +0100

    use hash router instead of browser router to fix hosted routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 import { PlayGame } from "./PlayGame";
 import { GameplayStats } from "./GameplayStats";
 
 export const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<PlayGame />} />
         <Route path="stats" element={<GameplayStats />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
